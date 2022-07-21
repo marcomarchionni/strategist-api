eef653dd20e3b75f6cc3d6f7243c5d1f0fe2ef2d
Marco <marco.marchionni@gmail.com>
2022-07-21 16:33:21 +0200

    TradeController Unit Test refactoring

--- src/main/java/com/marcomarchionni/ibportfolio/rest/TradeController.java
+++ src/main/java/com/marcomarchionni/ibportfolio/rest/TradeController.java
@@ -2,6 +2,7 @@ package com.marcomarchionni.ibportfolio.rest;
 
 import com.marcomarchionni.ibportfolio.models.Trade;
 import com.marcomarchionni.ibportfolio.services.TradeService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,8 +18,10 @@ public class TradeController {
     }
 
     @GetMapping("/trades")
-    public List<Trade> getTrades(@RequestParam (value = "startDate", required = false) LocalDate startDate,
-                                 @RequestParam (value = "endDate", required = false) LocalDate endDate,
+    public List<Trade> getTrades(@RequestParam (value = "startDate", required = false)
+                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                 @RequestParam (value = "endDate", required = false)
+                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
                                  @RequestParam (value = "tagged", required = false) Boolean tagged,
                                  @RequestParam (value = "symbol", required = false) String symbol,
                                  @RequestParam (value = "assetCategory", required = false) String assetCategory
--- src/test/java/com/marcomarchionni/ibportfolio/rest/TradeControllerTest.java
+++ src/test/java/com/marcomarchionni/ibportfolio/rest/TradeControllerTest.java
@@ -1,8 +1,8 @@
 package com.marcomarchionni.ibportfolio.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.marcomarchionni.ibportfolio.models.Trade;
-import com.marcomarchionni.ibportfolio.rest.exceptionhandling.exceptions.EntityNotFoundException;
 import com.marcomarchionni.ibportfolio.services.TradeService;
 import com.marcomarchionni.ibportfolio.util.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,22 +13,14 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -36,25 +28,28 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 class TradeControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    ObjectMapper mapper;
-
-    @MockBean
+    @Mock
     TradeService tradeService;
 
-    @Autowired
+    @InjectMocks
     TradeController tradeController;
+
+    ObjectMapper mapper;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mockMvc = MockMvcBuilders.standaloneSetup(tradeController).build();
+    }
 
     @Test
     void getTrades() throws Exception {
-
         List<Trade> trades = TestUtils.getSampleTrades();
 
         when(tradeService.findWithParameters(any(), any(), any(), any(), any())).thenReturn(trades);
@@ -73,7 +68,6 @@ class TradeControllerTest {
 
         when(tradeService.findWithParameters(any(),any(),any(),any(),any())).thenReturn(resultList);
 
-
         mockMvc.perform(get("/trades")
                         .param("startDate", startDate)
                         .param("endDate", endDate)
@@ -91,15 +85,13 @@ class TradeControllerTest {
     void getTradesWithParametersBadRequest(String startDate, String endDate, String tagged, String symbol, String assetCategory) throws Exception {
 
         mockMvc.perform(get("/trades")
-                        .param("symbol", symbol)
                         .param("startDate", startDate)
                         .param("endDate", endDate)
                         .param("tagged", tagged)
+                        .param("symbol", symbol)
                         .param("assetCategory", assetCategory))
                 .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.status", is(400)));
+                .andExpect(status().is4xxClientError());
     }
 
     @Test
@@ -110,27 +102,10 @@ class TradeControllerTest {
         when(tradeService.updateStrategyId(trade)).thenReturn(trade);
 
         mockMvc.perform(put("/trades")
-                .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(trade)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.symbol", is(trade.getSymbol())));
-    }
-
-    @Test
-    void updateStrategyIdEntityNotFoundTest() throws Exception {
-
-        Trade trade = TestUtils.getSampleTrade();
-
-        when(tradeService.updateStrategyId(trade)).thenThrow(new EntityNotFoundException("Trade or Strategy not found"));
-
-        mockMvc.perform(put("/trades")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(trade)))
-                .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.status", is(404)))
-                .andExpect(jsonPath("$.message", is("Trade or Strategy not found")));
     }
 }
