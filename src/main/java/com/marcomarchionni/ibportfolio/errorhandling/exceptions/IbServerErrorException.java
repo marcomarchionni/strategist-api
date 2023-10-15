657a09b4f12f178923cbf91c71ac3121d0f2f0ea
Marco Marchionni <marco.marchionni@gmail.com>
2023-10-15 08:56:11 +0200

    Fixed xml conversion configuration

--- src/main/java/com/marcomarchionni/ibportfolio/errorhandling/exceptions/IbServerErrorException.java
+++ src/main/java/com/marcomarchionni/ibportfolio/errorhandling/exceptions/IbServerErrorException.java
@@ -2,10 +2,6 @@ package com.marcomarchionni.ibportfolio.errorhandling.exceptions;
 
 public class IbServerErrorException extends RuntimeException {
 
-    public IbServerErrorException(String message) {
-        super(message);
-    }
-
     public IbServerErrorException() {
         super("Error while invoking external parsing");
     }
